76f2d86086a5371b1683c79bbfb0592fce53caef
Guilherme Labrego <glabrego@gmail.com>
2016-10-08 22:33:08 -0300

    First Kill

---
+++ config/application.rb
@@ -0,0 +1,16 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module MomentosEmArte
+  class Application < Rails::Application
+    config.active_record.primary_key = :uuid
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+  end
+end
